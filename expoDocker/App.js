0ef4803d5177bd3fb29a62b445bfb19d01990774
EC2 Default User <ec2-user@ip-172-31-27-202.us-west-2.compute.internal>
2019-05-29 11:23:45 +0000

    Add previous project with AWSMobile.

---
+++ expoDocker/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import Amplify, {Auth} from 'aws-amplify';
+import AWSConfig from './aws-exports';
+Amplify.configure(AWSConfig);
+
+export default class App extends React.Component {
+  state = {
+    username:'',
+    password:'',
+    phone_number:'',
+    email:'',
+    confirmationCode:''
+  }
+  onChangeText(key, value) {
+    this.setState({
+      [key]: value
+    });
+  }
+  signUp() {
+    Auth.signUp({
+      username: this.state.username,
+      password: this.state.password,
+      attributes: {
+      phone_number: this.state.phone_number,
+      email: this.state.email, 
+      }
+      })
+      .then(() => console.log('successful sign up!'))
+      .catch(err => console.log('error sign up: ', err));
+  } 
+  confirmSignUp() {
+    Auth.confirmSignUp(this.state.username, this.state.confirmationCode)
+    .then(() => console.log('successful confirm sign up!'))
+    .catch(err => console.log('error confirm sign up: ', err));
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={value => this.onChangeText('username', value)}
+          style={styles.input}
+          placeholder='Username'
+        />
+        <TextInput
+          onChangeText={value => this.onChangeText('password', value)}
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder='Password'
+        />
+        <TextInput
+          onChangeText={value => this.onChangeText('phone_number', value)}
+          style={styles.input}
+          placeholder='Phone Number'
+        />
+        <TextInput
+          onChangeText={value => this.onChangeText('email', value)}
+          style={styles.input}
+          placeholder='Email'
+        />
+        <Button title="Sign Up" onPress={this.signUp.bind(this)} />
+        
+        <TextInput
+          onChangeText={value => this.onChangeText('confirmationCode', value)}
+          style={styles.input}
+          placeholder='Confirmation Code'
+        />
+        <Button title="Confirm Sign Up" onPress={this.confirmSignUp.bind(this)} />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+ input: {
+    height: 50,
+    width: 200
+    //, borderBottomWidth: 2,
+    // borderBottomColor: "#2196F3"
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
